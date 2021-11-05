53adb1cd5dafa70b7cd7921ca8ea9e233e4c77e3
houzairkoussa <houzairmk@icloud.com>
2021-11-05 14:36:59 +0400

    create silo

--- app/controllers/playlists_controller.rb
+++ app/controllers/playlists_controller.rb
@@ -4,6 +4,21 @@ class PlaylistsController < ApplicationController
   end
 
   def new
-    @platlist = Playlist.new
+    @playlist = Playlist.new
+  end
+
+  def create
+    @playlist = Playlist.new(playlist_params)
+    if @playlist.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def playlist_params
+    params.require(:playlist).permit(:name, :genre, :description)
   end
 end
